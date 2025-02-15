e184051a01eb69f61a2f92c44e56e151e0cf8dc3
raychaser <terminadev31@gmail.com>
2025-02-15 12:32:49 +0400

    fix floaterm

--- lua/r8ch/remap.lua
+++ lua/r8ch/remap.lua
@@ -9,5 +9,5 @@ vim.keymap.set("n", "<leader>f", ":lua vim.lsp.buf.format()\n")
 vim.keymap.set("n", "<leader>D", ":lua require'telescope.builtin'.diagnostics{}\n<Esc>")
 vim.keymap.set("n", "<leader>R", vim.lsp.buf.references);
 vim.keymap.set("n", "<leader>h", ":nohlsearch\n")
-vim.keymap.set("n", "<leader>tt", ":FloatermNew! --height=0.8 --width=0.9<CR>")
+vim.keymap.set("n", "<leader>tt", ":FloatermNew --height=0.8 --width=0.9 pwsh<CR>cls<CR>")
 vim.api.nvim_create_user_command("LinesCountCS", '!git ls-files \"*.cs\" | xargs wc -l', {});
