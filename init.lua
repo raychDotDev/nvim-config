f1859663a6b7e4157c06da43912653581388b7c7
raychDotDev <raychaser29@gmail.com>
2025-07-20 21:43:58 +0400

    add wsl clipboard support

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 
-
 --PLUGINS
 require("config.lazy")
 
--- lua/config/misc.lua
+++ lua/config/misc.lua
@@ -7,6 +7,23 @@ vim.opt.number = true
 vim.opt.cursorline = false
 vim.opt.showmode = false
 vim.opt.clipboard = 'unnamedplus'
+
+local clip = "/mnt/c/Windows/System32/clip.exe" -- Change this path if needed
+
+if vim.fn.executable(clip) then
+  local opts = {
+    callback = function()
+      if vim.v.event.operator ~= "y" then
+        return
+      end
+      vim.fn.system(clip, vim.fn.getreg(0))
+    end,
+  }
+
+  opts.group = vim.api.nvim_create_augroup("WSLYank", {})
+  vim.api.nvim_create_autocmd("TextYankPost", { group = opts.group, callback = opts.callback })
+end
+
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
