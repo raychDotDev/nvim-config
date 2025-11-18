89774722732109d65e7c5b897a0c8b8087eb6e83
raychDotDev <raychaser29@gmail.com>
2025-11-19 00:14:07 +0400

    neotree float preview thingy

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -12,16 +12,25 @@ return {
 			{ "<leader>e", "<cmd>:Neotree toggle float<cr>", desc = "File explorer" },
 		},
 		opts = {
-			enable_git_status = true,
-			git_status_async = true,
-			use_popups_for_input = false,
+			use_popups_for_input = true,
 			event_handlers = {
 				{
 					event = "file_opened",
 					handler = function(_)
 						require("neo-tree.command").execute({ action = "close" })
 					end,
+
 				},
+				{
+					event = 'after_render',
+					handler = function()
+						local state = require('neo-tree.sources.manager').get_state('filesystem')
+						if not require('neo-tree.sources.common.preview').is_active() then
+							state.config = { use_float = true} -- or whatever your config is
+							state.commands.toggle_preview(state)
+						end
+					end
+				}
 			}
 		}
 	}
